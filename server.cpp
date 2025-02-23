47ed843436a72b98ee0e08168e8ce05ef49ac47a
ahmed ghounami <ahmedghounami0@gmail.com>
2025-02-23 19:44:06 +0000

    start webserve

--- server.cpp
+++ server.cpp
@@ -1,22 +1,4 @@
-#include <iostream>
-#include <string>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <poll.h>
-#include <fstream>
-#include <vector>
-#include <map>
-
-struct client_info
-{
-    std::string chunk;
-};
-
-std::map<int, client_info> clients;
-
-#define PORT 8080
+#include "server.hpp"
 
 int main()
 {
