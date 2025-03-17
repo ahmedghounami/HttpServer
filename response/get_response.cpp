0eb4250b076f787c3d3087064a9ebc7f4b1e96cf
aghounam <ahmedghounami0@gmail.com>
2025-03-17 17:56:37 +0000

    test

---
+++ response/get_response.cpp
@@ -0,0 +1,10 @@
+#include "../server.hpp"
+
+void handleGetRequest(client_info &client, std::map<int, server_config> &server)
+{
+    (void)server;
+    std::cout << client.boundary << std::endl;
+    std::cout << client.method << std::endl;
+    std::cout << client.uri << std::endl;
+    std::cout << client.version << std::endl;
+}
