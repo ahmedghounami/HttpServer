1bce5b829ff1d07d4286213d62d3b646e2d6a685
Mehdi Kibous <mkibous@e2r4p7.1337.ma>
2025-04-18 21:42:29 +0100

    php cgi work whit time out

--- HandlingRequest/handleRequest.cpp
+++ HandlingRequest/handleRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: mkibous <mkibous@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 22:39:03 by hboudar           #+#    #+#             */
-/*   Updated: 2025/04/17 22:23:34 by mkibous          ###   ########.fr       */
+/*   Updated: 2025/04/18 21:41:10 by mkibous          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 //  Because GET requests are cacheable, the caching behavior is defined
 //  in related RFCs (e.g., RFC 7234). This means that responses to GET
 //  requests can be stored and reused under the right conditions.
+
 std::string getContentType(const std::string &path)
 {
     std::string extension = path.substr(path.find_last_of(".") + 1);
@@ -76,28 +77,33 @@ int findMatchingServer(client_info &client, std::map<int, server_config> &server
     // std::cout << "Server config found for host: " << host << " and port: " << port << "in server config index: " << server_index << std::endl;
     return server_index;
 }
-void success(client_info &client, std::string &body, std::string &path, bool whith_header = true)
+void success(client_info &client, std::string &body, std::string &path, std::string content_type = "", bool whith_header = true, double file_size = -1)
 {
-
+    if( content_type == "")
+        content_type = getContentType(path);
     client.poll_status = 1;
     if (whith_header)
     {
-        std::ifstream file(path.c_str());
-        if (!file.is_open())
+        if(file_size == -1)
         {
-            return;
+            std::ifstream file(path.c_str());
+            if (!file.is_open())
+            {
+                return;
+            }
+            file.seekg(0, std::ios::end);
+            file_size = file.tellg();
+            file.seekg(0, std::ios::beg);
+            // std::cout << "file size: " << file_size << std::endl;
         }
-        file.seekg(0, std::ios::end);
-        int file_size = file.tellg();
-        file.seekg(0, std::ios::beg);
-        std::cout << "file size: " << file_size << std::endl;
         client.response = "HTTP/1.1 200 OK\r\n";
-        client.response += "Content-Type: " + getContentType(path) + "\r\n";
+        client.response += "Content-Type: " + content_type + "\r\n";
         client.response += "Content-Length: " + std::to_string(file_size) + "\r\n";
         client.response += "Connection: keep-alive\r\n";
         client.response += "\r\n";
         client.bytes_sent = ((double)client.response.size()  * -1) - 1;
         std::cout << "response size: " << client.bytes_sent << std::endl;
+        std::cout << "file size: " << file_size << std::endl;
         // sleep(10);
     }
     else
@@ -137,21 +143,32 @@ std::string getcorectserver_path(client_info &client, std::map<int, server_confi
         return server[server_index].locations[loc].path;
     return server[server_index].path;
 }
+// void timeout_handler(int signum) {
+//     exit(1);  // or set a flag
+// }
 void handleGetRequest(client_info &client, std::map<int, server_config> &server)
 {
     std::cout << "in get funciton" << std::endl;
     client.response.clear();
+    std::string content_type = "";
+    // bool is_php = false;
     // int server_index = findMatchingServer(client, server);
 
     // std::cout << "server path: " << server[server_index].path << std::endl;
     // std::cout << "client path: " << client.uri << std::endl;
     // std::string loc = getlocation(client, server[server_index]);
     std::string body;
+    long content_size = -1;
+    // FILE *fp;
 
     std::string path = getcorectserver_path(client, server) + client.uri;
 
-    std::cout << "path: " << path << std::endl;
-
+    // std::cout << "path: " << path << std::endl;
+    if(path.find_last_of("/") == path.size() - 1)
+    {
+        path += "index.html";
+        // std::cout << "path: " << path << std::endl;
+    }
     // try to open the file
     bool whith_header = 0;
     std::ifstream file(path.c_str());
@@ -184,13 +201,196 @@ void handleGetRequest(client_info &client, std::map<int, server_config> &server)
         // std::cout << "file open" << std::endl;
         // return;
     // }
+    if(path.find_last_of(".") != std::string::npos)
+    {
+        std::cout << "in php file" << std::endl;
+        if(path.substr(path.find_last_of(".") + 1) == "php")
+        {
+            int fd[2];
+            if (pipe(fd) == -1)
+            {
+                std::cerr << "pipe failed" << std::endl;
+                return;
+            }
+            pid_t pid = fork();
+            if (pid == -1)
+            {
+                std::cerr << "fork failed" << std::endl;
+                return;
+            }
+            else if (pid == 0)
+            {
+                // child process
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[0]);
+                close(fd[1]);
+                // std::string command = "/Users/mkibous/Desktop/webserver/www/php-cgi " + path;
+                // std::cout << "command: " << command << std::endl;
+                char *args[] = {(char *)"/Users/mkibous/Desktop/webserver/www/php-cgi", (char *)path.c_str(), NULL};
+                // std::cout << "in php file2" << std::endl;
+                alarm(5); // set timeout to 5 seconds
+                execv(args[0], args);
+                exit(0);
+            }
+            else
+            {
+                std::cout << "in php file3" << std::endl;
+                // parent process
+                close(fd[1]);
+                char buffer[1024];
+                int bytes_read;
+                std::string headers;
+                //readline by line until we find \r\n\r\n
+                while ((bytes_read = read(fd[0], buffer, sizeof(buffer))) > 0)
+                {
+                    std::cout << "bytes read: " << bytes_read << std::endl;
+                    headers += std::string(buffer, bytes_read);
+                    if (headers.find("\r\n\r\n") != std::string::npos){
+                        //remove what is after \r\n\r\n
+                        size_t pos = headers.find("\r\n\r\n");
+                        //calculate the size of what is after \r\n\r\n
+                        size_t size = headers.size() - pos - 4;
+                        body = headers.substr(pos + 4, size);
+                        headers = headers.substr(0, pos + 4);
+                        break;}
+                    
+                }
+                std::cout << "bytes sent: " << client.bytes_sent << std::endl;
+                if(client.bytes_sent <= 0 && client.bytes_sent != -1 )
+                {
+                    std::ofstream headers_file("headers.txt");
+                    if (headers_file.is_open())
+                    {
+                        std::cout << "headers: " << std::endl;
+                        headers_file << headers;
+                        headers_file.close();
+                    }
+                    while ((bytes_read = read(fd[0], buffer, sizeof(buffer))) > 0){
+                        body+= std::string(buffer, bytes_read);
+                        
+                    }
+                    std::cout << "body size: " << body.size() << std::endl;
+                    if(headers.size() > 0)
+                        content_type = headers.substr(headers.find("Content-Type: ") + 14, headers.find("\r\n", headers.find("Content-Type: ")) - headers.find("Content-Type: ") - 14);
+                    // if(content_type == "")
+                    //     content_type = "text/html";
+                    //openfile ant write to it
+                    // std::ofstream body_file("body.txt");
+                    // if (body_file.is_open())
+                    // {
+                    //     body_file << body;
+                    //     body_file.close();
+                    // }
+                    wait(NULL);
+                    std::cout << "content type: " << content_type << std::endl;
+                    // exit(0);
+                    alarm(0);
+                    close(fd[0]);
+                    if(content_type == "")
+                    {
+                        unknown_error(client);
+                        return ;
+                        
+                    }
+                    success(client, body, path, content_type, true, body.size());
+                    return;
+                }
+                else if(client.bytes_sent == -1)
+                    client.bytes_sent = 0;
+                while  (body.size() < client.bytes_sent && (bytes_read = read(fd[0], buffer, sizeof(buffer))) > 0 )
+                {
+                    body += std::string(buffer, bytes_read);
+                    // std::cout << "body size: " << body.size() << std::endl;
+                    
+                }
+                if(body.size() >= client.bytes_sent)
+                {
+                    std::string temp = body.substr(client.bytes_sent, body.size() - client.bytes_sent);
+                    body = temp;
+                    // std::cout << "body size: " << body.size() << std::endl;
+                }
+                while (body.size() < READ_BUFFER_SIZE && (bytes_read = read(fd[0], buffer, sizeof(buffer))) > 0)
+                {
+                    body += std::string(buffer, bytes_read);
+                    // std::cout << "body size: " << body.size() << std::endl;
+                    
+                }
+                if(bytes_read == 0)
+                {
+                    std::cout << "file end reached" << std::endl;
+                    client.datafinished = 1;
+                }
+                // wait(NULL);
+                alarm(0);
+                close(fd[0]);
+                success(client, body, path, content_type, false);
+                return;
+            }
+        }
+        
+    }
     if(client.bytes_sent <= 0 && client.bytes_sent != -1 )
     {
-        success(client, body, path, true);
+        success(client, body, path, content_type, true, content_size);
         return;
     }
     else if(client.bytes_sent == -1)
         client.bytes_sent = 0;
+    // if(is_php)
+    // {
+    //     std::cout << "in php file3" << std::endl;
+    //     std::string command = "/Users/mkibous/Desktop/webserver/www/php-cgi " + path;
+    //     fp = popen(command.c_str(), "r");
+    //     if (fp == NULL)
+    //     {
+    //         std::cerr << "Failed to run command: " << command << std::endl;
+    //         return;
+    //     }
+    //     char buffer[128];
+    //     //skip headers
+    //     std::string headers;
+    //     while (fread(buffer, 1, sizeof(buffer), fp) > 0)
+    //     {
+    //         headers += buffer;
+    //         if (headers.find("\r\n\r\n") != std::string::npos)
+    //             break;
+    //     }
+    //     // std::cout << "bytes sent: " << client.bytes_sent << std::endl;
+    //     while(fread(buffer, 1, sizeof(buffer), fp) > 0 && body.size() < client.bytes_sent)
+    //     {
+    //         body += buffer;
+    //         // std::cout << "body size: " << body.size() << std::endl;
+    //     }
+    //     std::cout <<"bytes sent: " << client.bytes_sent << std::endl;
+    //     body.clear();
+    //     std::cout << "in php file4" << std::endl;
+    //     while(fread(buffer, 1, sizeof(buffer), fp) > 0 && body.size() < READ_BUFFER_SIZE)
+    //     {
+    //         body += buffer;
+    //         // std::cout << "body size: " << body.size() << std::endl;
+    //     }
+    //     // std::cout << "in php file5" << std::endl;
+    //     // std::ofstream body_file("body.txt");
+    //     // if (body_file.is_open())
+    //     // {
+    //     //     body_file << body;
+    //     //     body_file.close();
+    //     // }
+    //     std::cout << "body size: " << body.size() << std::endl;
+    //     // exit(0);
+    //     // if (feof(fp))
+    //     //     client.datafinished = 1, std::cout << "file end reached" << std::endl;
+    //     //check if file end is reached
+    //     if(feof(fp))
+    //     {
+    //         std::cout << "file end reached" << std::endl;
+    //         client.datafinished = 1;
+    //     }
+    //     success(client, body, path, content_type, false);
+    //     pclose(fp);
+    //     // exit(0);
+    //     return;
+    // }
     char *buffer = new char[READ_BUFFER_SIZE];
     // read only 1024 bytes
     file.seekg(0, std::ios::beg);                 // move to the beginning
@@ -208,7 +408,7 @@ void handleGetRequest(client_info &client, std::map<int, server_config> &server)
     // file.seekg(0, std::ios::beg); to go to the beginning
     // file.seekg(-400, std::ios::cur); to go back 400 bytes
 
-    success(client, body, path, whith_header);
+    success(client, body, path, content_type, whith_header);
     // file.close();
 
     std::cout << "data finished: " << client.datafinished << std::endl;
