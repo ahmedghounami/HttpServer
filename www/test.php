6727798883186cb3e9c87da1bfeafc5a52b92819
Mehdi Kibous <mkibous@e2r5p8.1337.ma>
2025-04-22 16:38:04 +0100

    cgi env fixed

---
+++ www/test.php
@@ -0,0 +1,13 @@
+<?php
+// Set the content type header
+header("Content-Type: image/png");
+
+// Path to the image file
+
+readfile("./image2.png");
+// while (true) {
+//     // Do nothing, just keep the script running
+// }
+// Check if the image was read successfully
+?>
+
